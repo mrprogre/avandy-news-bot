772c2512520eb0817a68255913d3ba5acb34e6b3
Dmitry <rps_project@mail.ru>
2023-12-15 11:43:48 +0300

    cron to 5 min

--- src/main/java/com/avandy/bot/service/Search.java
+++ src/main/java/com/avandy/bot/service/Search.java
@@ -38,7 +38,7 @@ public class Search {
         this.newsListRepository = newsListRepository;
     }
 
-    @Scheduled(cron = "0 */1 * * * ?")
+    @Scheduled(cron = "0 */5 * * * ?")
     public void scheduler() {
         long start = System.currentTimeMillis();
 
