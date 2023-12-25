42160ca153ae545a3f6d33353e1185a9c55a5d77
Dmitry <rps_project@mail.ru>
2023-12-25 18:05:54 +0300

    ref

--- src/main/resources/selects.sql
+++ src/main/resources/selects.sql
@@ -16,3 +16,9 @@ where u.chat_id != 1254981379;
 -- todo Сделать тесты на каждый кейс в идеале
 -- todo Сделать поиск по ключевым словам с %, чтобы искать типа повыш%зарплат'
 -- todo проверка заблокирован ли бот юзером
+
+select n.source, n.title, extract(minute from (n.pub_date - n.add_date)) as "pub-add"
+from news_list n
+         join showed_news s
+              on n.title_hash = s.title_hash and s.chat_id = 6190696388
+order by n.id desc;
