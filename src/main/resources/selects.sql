471f42aa52945223150c377ff2baff3297f7823b
Dmitry <rps_project@mail.ru>
2024-01-06 21:28:55 +0300

    ref

--- src/main/resources/selects.sql
+++ src/main/resources/selects.sql
@@ -15,20 +15,20 @@ select u.chat_id,
 from users u
          left join settings s on u.chat_id = s.chat_id
          left join keywords k on u.chat_id = k.chat_id
-         left join excluded e on u.chat_id = e.chat_id
+         left join excluding_terms e on u.chat_id = e.chat_id
 where u.chat_id not in (1254981379 /* я */ /*,1020961767 /* Лена */, 6455565758 /* Вика */, 6128707071 /* Саша */*/)
 group by u.chat_id, u.first_name, s.period, s.period_all, s.scheduler, s.start, s.excluded, s.lang, s.period_top,
          u.is_active;
 
 -- Rows count
 -- 05.01.2024: 2747, 25, 25470, 32, 5291, 205, 9
-select (select count(*) from excluded)         as excluded,
-       (select count(*) from keywords)         as keywords,
-       (select count(*) from news_list)        as news_list,
-       (select count(*) from rss_list)         as rss_list,
-       (select count(*) from showed_news)      as showed_news,
-       (select count(*) from top_ten_excluded) as top_ten_excluded,
-       (select count(*) from users)            as users;
+select (select count(*) from excluding_terms) as excluded,
+       (select count(*) from keywords)        as keywords,
+       (select count(*) from news_list)       as news_list,
+       (select count(*) from rss_list)        as rss_list,
+       (select count(*) from showed_news)     as showed_news,
+       (select count(*) from top_excluded)    as top_ten_excluded,
+       (select count(*) from users)           as users;
 
 -- Новости, которые получил пользователь
 select source, title, pub_date, extract(minute from (pub_date - add_date)) as "pub-add"
