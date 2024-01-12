0f48a3faa5fd0f625bdaf3a97d1bf48d9db547e0
Dmitry <rps_project@mail.ru>
2024-01-12 14:09:31 +0300

    add auto cleanup show_news

--- src/main/java/com/avandy/bot/repository/ShowedNewsRepository.java
+++ src/main/java/com/avandy/bot/repository/ShowedNewsRepository.java
@@ -1,7 +1,9 @@
 package com.avandy.bot.repository;
 
 import com.avandy.bot.model.ShowedNews;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -10,5 +12,11 @@ public interface ShowedNewsRepository extends JpaRepository<ShowedNews, Long> {
 
     @Query(value = "SELECT title_hash FROM showed_news WHERE chat_id = :chatId", nativeQuery = true)
     List<String> findShowedNewsHashByChatId(Long chatId);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "DELETE FROM showed_news WHERE add_date < (current_timestamp - cast('72 hours' as interval))",
+            nativeQuery = true)
+    int deleteOldShowedNews();
 
 }
--- src/main/java/com/avandy/bot/utils/Monitoring.java
+++ src/main/java/com/avandy/bot/utils/Monitoring.java
@@ -1,35 +1,43 @@
 package com.avandy.bot.utils;
 
 import com.avandy.bot.repository.NewsListRepository;
+import com.avandy.bot.repository.ShowedNewsRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-
+@RequiredArgsConstructor
 public class Monitoring {
-    private NewsListRepository newsListRepository;
+    private final NewsListRepository newsListRepository;
+    private final ShowedNewsRepository showedNewsRepository;
 
-    @Autowired
-    public void setNewsListRepository(NewsListRepository newsListRepository) {
-        this.newsListRepository = newsListRepository;
-    }
-
-
+    // error: Мониторинг работы загрузчика новостей: раз в 2 часа
     @Scheduled(cron = "${cron.monitoring.no.save.news}")
     void getStatNoSaveNews() {
-        if (newsListRepository.getNewsListCountBy6Hours("2 hours") < 10) {
-            log.error("За 2 часа не сохранено ни одной новости! Проверить!");
+        int newsListCounts = newsListRepository.getNewsListCountBy6Hours("2 hours");
+        if (newsListCounts < 10) {
+            log.error("За 6 часов сохранено новостей: {}. Проверить!", newsListCounts);
         }
     }
 
+    // warn: Мониторинг количества загруженных новостей: раз в 6 часов
     @Scheduled(cron = "${cron.monitoring.save.news}")
     void getStatSaveNews() {
-        int newsListCountBy6Hours = newsListRepository.getNewsListCountBy6Hours("6 hours");
-        if (newsListCountBy6Hours > 0) {
-            log.warn("За 6 часов сохранено {} новостей!", newsListCountBy6Hours);
+        int newsListCounts = newsListRepository.getNewsListCountBy6Hours("6 hours");
+        if (newsListCounts > 0) {
+            log.warn("За 6 часов сохранено новостей: {} ", newsListCounts);
+        }
+    }
+
+    // info: Очистка таблицы showed_news раз в сутки в 6 утра
+    @Scheduled(cron = "${cron.delete.old.showed.news}")
+    void deleteOldShowedNews() {
+        int newsListCounts = showedNewsRepository.deleteOldShowedNews();
+        if (newsListCounts > 0) {
+            log.warn("Очистка таблицы showed_news, удалено: {} новостей!", newsListCounts);
         }
     }
 }
