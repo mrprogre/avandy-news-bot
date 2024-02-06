ce95c384e8118697650f945697f54ca2a60e7624
Dmitry <rps_project@mail.ru>
2024-02-06 18:23:58 +0300

    add test

---
+++ src/test/java/com/avandy/bot/AvandyNewsBotApplicationTest.java
@@ -0,0 +1,13 @@
+package com.avandy.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AvandyNewsBotApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
