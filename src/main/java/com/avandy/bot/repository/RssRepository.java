98fa2318971136946f1301207a204c79e21244dc
Dmitry <rps_project@mail.ru>
2024-02-09 21:16:49 +0300

    ref

--- src/main/java/com/avandy/bot/repository/RssRepository.java
+++ src/main/java/com/avandy/bot/repository/RssRepository.java
@@ -9,29 +9,29 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RssRepository extends CrudRepository<RssList, Integer> {
-    @Query(value = "SELECT DISTINCT source FROM rss_list WHERE is_active = 1 " +
-            "AND (lang = :lang or chat_id = :chatId) ORDER BY source", nativeQuery = true)
+    @Query(value = "select distinct source from rss_list where is_active = 1 " +
+            "and (lang = :lang or chat_id = :chatId) order by source", nativeQuery = true)
     List<String> findAllActiveSources(String lang, long chatId);
 
     @Query(value = "SELECT source FROM rss_list WHERE is_active = 1 and chat_id = :chatId ORDER BY source",
             nativeQuery = true)
     List<String> findPersonalSources(long chatId);
 
-    @Query(value = "SELECT 1 FROM rss_list WHERE chat_id = :chatId and link = :link", nativeQuery = true)
+    @Query(value = "select 1 from rss_list where chat_id = :chatId and link = :link", nativeQuery = true)
     Integer isExistsByChatId(long chatId, String link);
 
-    @Query(value = "SELECT 1 FROM rss_list WHERE chat_id = :chatId and lower(source) = lower(:source)", nativeQuery = true)
+    @Query(value = "select 1 from rss_list where chat_id = :chatId and lower(source) = lower(:source)", nativeQuery = true)
     Integer isExistsBySourceName(long chatId, String source);
 
-    @Query(value = "FROM rss_list WHERE isActive = 1 and parserType = 'rss'")
+    @Query(value = "select * from rss_list where is_active = 1 and parser_type = 'rss'", nativeQuery = true)
     List<RssList> findAllActiveRss();
 
-    @Query(value = "FROM rss_list WHERE isActive = 1 and parserType <> 'rss'")
+    @Query(value = "select * from rss_list where is_active = 1 and parser_type <> 'rss'", nativeQuery = true)
     List<RssList> findAllActiveNoRss();
 
     @Transactional
     @Modifying(clearAutomatically = true)
-    @Query(value = "UPDATE rss_list SET is_active = :value WHERE link = :link", nativeQuery = true)
+    @Query(value = "update rss_list set is_active = :value where link = :link", nativeQuery = true)
     int updateIsActiveRss(int value, String link);
 
     @Transactional
