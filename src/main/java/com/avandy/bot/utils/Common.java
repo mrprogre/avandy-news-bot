2b9caae5ff74beda825c67d9e88eaf36f6441d45
Dmitry <rps_project@mail.ru>
2024-02-20 11:08:24 +0300

    ref max excl terms = 20 from 50

--- src/main/java/com/avandy/bot/utils/Common.java
+++ src/main/java/com/avandy/bot/utils/Common.java
@@ -23,7 +23,7 @@ public class Common {
     public static final int LIMIT_FOR_BREAKING_INTO_PARTS = 50;
     public static final int MAX_KEYWORDS_COUNT_PREMIUM = 100;
     public static final int MAX_KEYWORDS_COUNT = 5;
-    public static final int MAX_EXCL_TERMS_COUNT = 55;
+    public static final int MAX_EXCL_TERMS_COUNT = 20;
     public static final String ERROR_TEXT = "Error occurred: ";
     public static final String ICON_SEARCH = "\uD83C\uDFB2";
     public static final String ICON_NEWS_FOUNDED = "\uD83C\uDF3F";
